653c983e62afec195b9b24964bc4c2f6dcf591cc
Jeremy Sager <jeremy@whiteboardfederal.com>
2020-01-21 23:31:14 -0500

    Initial commit of a slightly configured initializr version

---
+++ src/main/java/com/whiteboardfederal/resumaker/ResumakerApplication.java
@@ -0,0 +1,13 @@
+package com.whiteboardfederal.resumaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whiteboardfederal/resumaker/ResumakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whiteboardfederal.resumaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

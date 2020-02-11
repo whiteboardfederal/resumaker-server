0a5d714de7dd7a0d3efc008cd3f26aadab00ade4
DougWBF <dfrederick@whiteboardfederal.com>
2020-02-11 18:52:32 -0500

    Moved files, need to fix package paths probably

--- src/main/java/com/whiteboardfederal/resumaker/repository/EducationRepository.java
+++ src/main/java/com/whiteboardfederal/resumaker/repository/EducationRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.whiteboardfederal.resumaker.model.Education;
 
 public interface EducationRepository extends JpaRepository<Education, Long> {
-
   // Find all employees with a certain last name
   public List<Education> findByEmployeeId(Long employeeId);
 }

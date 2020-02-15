92aa97667c262305367b4679842f11bda6bbc769
Jarrett Horton <jghorton14@gmail.com>
2020-02-15 18:06:46 -0500

    Removed extra methods in Education Repo #14

--- src/main/java/com/whiteboardfederal/resumaker/repository/EducationRepository.java
+++ src/main/java/com/whiteboardfederal/resumaker/repository/EducationRepository.java
@@ -1,12 +1,8 @@
 package com.whiteboardfederal.resumaker.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.whiteboardfederal.resumaker.model.Education;
 
 public interface EducationRepository extends JpaRepository<Education, Long> {
-  // Find all employees with a certain last name
-  public List<Education> findByEmployeeId(Long employeeId);
 }

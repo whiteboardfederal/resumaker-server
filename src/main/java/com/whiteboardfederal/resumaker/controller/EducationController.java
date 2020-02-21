e8681a0f7dfc964e0d947090b58596389d852c2c
Jarrett Horton <jghorton14@gmail.com>
2020-02-20 22:25:25 -0500

    Added swagger to Employee, Education, and work history #21

--- src/main/java/com/whiteboardfederal/resumaker/controller/EducationController.java
+++ src/main/java/com/whiteboardfederal/resumaker/controller/EducationController.java
@@ -1,7 +1,10 @@
 package com.whiteboardfederal.resumaker.controller;
 
 import java.util.List;
-import java.util.Map;
+
+import com.whiteboardfederal.resumaker.model.Education;
+import com.whiteboardfederal.resumaker.repository.EducationRepository;
+import com.whiteboardfederal.resumaker.utils.EntityMissingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,15 +16,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.whiteboardfederal.resumaker.model.Education;
-import com.whiteboardfederal.resumaker.repository.EducationRepository;
-import com.whiteboardfederal.resumaker.utils.EntityMissingException;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -31,27 +32,52 @@ class EducationController {
   @Autowired
   private EducationRepository educationRepository;
 
+  @ApiOperation(value = "Allows you to find all the education entities.")
+  @ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved all education entities."),
+      @ApiResponse(code = 401, message = "You are not authorized to view education entities"),
+      @ApiResponse(code = 403, message = "Accessing this education entities are forbidden"),
+      @ApiResponse(code = 404, message = "The education entities you were trying to reach are not found") })
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   List<Education> query() {
     return educationRepository.findAll();
   }
 
+  @ApiOperation(value = "Allows you to find the education entity provided by suppied ID.")
+  @ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved education entity."),
+      @ApiResponse(code = 401, message = "You are not authorized to view education entity"),
+      @ApiResponse(code = 403, message = "Accessing this education entity is forbidden"),
+      @ApiResponse(code = 404, message = "The education entity you were trying to reach is not found") })
   @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   Education get(@PathVariable final long id) {
     return educationRepository.findById(id).orElseThrow(() -> new EntityMissingException("education", id));
   }
 
+  @ApiOperation(value = "Allows you to create an education entity.")
+  @ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully created education entity."),
+      @ApiResponse(code = 401, message = "You are not authorized to create education entity"),
+      @ApiResponse(code = 403, message = "Creating this education entity is forbidden") })
   @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   Education create(@RequestBody Education p) {
     return educationRepository.save(p);
   }
 
+  @ApiOperation(value = "Allows you to update an education entity with provided id.")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "Successfully updated the education entity with provided id."),
+      @ApiResponse(code = 401, message = "You are not authorized to update education entity"),
+      @ApiResponse(code = 403, message = "Updating this education entity is forbidden"),
+      @ApiResponse(code = 404, message = "The education entity you were trying to reach is not found") })
   @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   Education overwrite(@RequestBody Education p, @PathVariable long id) {
     p.setId(id);
     return educationRepository.save(p);
   }
 
+  @ApiOperation(value = "Allows you to delete an education entity with provided id.")
+  @ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully deleted the education entity with provided id."),
+      @ApiResponse(code = 401, message = "You are not authorized to delete education entity"),
+      @ApiResponse(code = 403, message = "Deleting this education entity is forbidden"),
+      @ApiResponse(code = 404, message = "The education entity you were trying to reach is not found") })
   @DeleteMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
   @ResponseStatus(value = HttpStatus.OK)
   void delete(@PathVariable long id) {

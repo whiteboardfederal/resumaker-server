3fdd0f9e7d708e4122f6349a2ea3d03dba173bf0
jskipp <jamesskipp@gmail.com>
2020-03-09 17:10:57 -0400

    added repository annotation

--- src/main/java/com/whiteboardfederal/resumaker/repository/CertificationRepository.java
+++ src/main/java/com/whiteboardfederal/resumaker/repository/CertificationRepository.java
@@ -2,6 +2,8 @@ package com.whiteboardfederal.resumaker.repository;
 
 import com.whiteboardfederal.resumaker.model.Certification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CertificationRepository extends JpaRepository<Certification, Long> {
 }

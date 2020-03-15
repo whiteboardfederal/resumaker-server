b25a8b51bb40a21d43e3fa4140d23f99cfa70f83
mazukas <mazukas@gmail.com>
2020-03-15 15:35:59 -0400

    Backed out some relational code.  Updated application.properties so that we can connect to a MySQL database and persist data across sessions

--- src/main/java/com/whiteboardfederal/resumaker/model/RefSkill.java
+++ src/main/java/com/whiteboardfederal/resumaker/model/RefSkill.java
@@ -1,10 +1,10 @@
 package com.whiteboardfederal.resumaker.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -20,6 +20,7 @@ public class RefSkill {
 
   @NotNull(message = "Description of RefSkill")
   @Size(min = 1)
+  @Column(name="description")
   private String desc;
 
   public RefSkill() {

dc7688193415f6ef5bce29f37bd734b7cb9af4e0
vivle <vle@whiteboardfederal.com>
2020-03-15 20:23:45 -0400

    Making RefBaseEntity

--- src/main/java/com/whiteboardfederal/resumaker/model/RefDegree.java
+++ src/main/java/com/whiteboardfederal/resumaker/model/RefDegree.java
@@ -1,54 +1,10 @@
 package com.whiteboardfederal.resumaker.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.persistence.Table;
 
 @Entity
-public class RefDegree {
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private Long id;
-
-  @NotNull(message = "Must be associated with an employee")
-  private String value;
-
-  @Size(min = 2, max = 100)
-  private String description;
-
-  public RefDegree() {
-  }
-
-  public RefDegree(String value, String description) {
-    this.value = value;
-    this.description = description;
-  }
-
-  public Long getId() {
-    return this.id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getValue() {
-    return this.value;
-  }
-
-  public void setValue(String value) {
-    this.value = value;
-  }
-
-  public String getDescription() {
-    return this.description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
+@Table(name = "ref_degree")
+public class RefDegree extends RefBaseEntity{
 
 }
